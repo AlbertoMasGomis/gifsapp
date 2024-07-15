6e5986357f523e8b7ea168770dd9157bb7174732
Alberto Mas <alberto.mas@nunsys.com>
2024-07-15 10:47:36 +0200

    Finish app

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -10,7 +10,10 @@ export class GifsService {
   private apiKey: string = '3iJyap4EM5SJwBlDnyE5NZrc3tKjnvMP';
   private serviceUrl: string = 'https://api.giphy.com/v1/gifs';
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {
+    this.loadLocalStorage();
+    this.searchTag(this._tagsHistory[0]);
+  }
 
   get tagsHistory() {
     return [...this._tagsHistory];
@@ -45,5 +48,15 @@ export class GifsService {
     }
     this._tagsHistory.unshift(tag);
     this._tagsHistory = this._tagsHistory.splice(0, 10);
+    this.saveLocalStorage();
+  }
+
+  private saveLocalStorage(): void {
+    localStorage.setItem('history', JSON.stringify(this._tagsHistory));
+  }
+
+  private loadLocalStorage(): void {
+    if (!localStorage.getItem('history')) return;
+    this._tagsHistory = JSON.parse(localStorage.getItem('history')!);
   }
 }
